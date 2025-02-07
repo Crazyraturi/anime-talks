d7cfa5a81d3268e67e677ec1b74751e174725154
crazyraturi <subodhblogger966@gmail.com>
2025-02-07 14:11:43 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
-import React from 'react'
+import {React,useState,useEffect} from "react";
+import { useDispatch } from "react-redux";
+import authservice from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
-   
-   console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loding, setLoding] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <div>
-      <h1>hello! subodh</h1>
-    </div>
-  )
-}
+  useEffect(() => {
+    authservice.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => {
+        setLoding(false);
+      });
+  }, []);
 
-export default App
+  return  !loding ? (
+  <div className=" min-h-screen flex-wrap content-between bg-gray-200 ">
+  <div className="w-full block">
+    <Header/> 
+    <main>
+    <Outlet/>
+    </main>
+    <Footer/>
+
+  </div>
+  </div>
+
+  ): null
+};
+
+export default App;
+
